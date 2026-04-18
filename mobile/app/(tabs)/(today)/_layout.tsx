1ce6acafdd347b47fef40f5deb8cf167d7e4ebcd
xmustafa5 <musmoh73@gmail.com>
2026-04-18 12:39:55 +0300

    gfgf

--- mobile/app/(tabs)/(today)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from 'expo-router/stack';
-
-export default function TodayLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: 'Daily Tracker' }} />
-    </Stack>
-  );
-}
